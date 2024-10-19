9c070b574ef5a9da1778e24fcf5b83de07286cbd
zericiia <karimbrahimi102@gmail.com>
2024-10-19 06:40:43 +0100

    use products from producs.js

--- javascript-amazon-project/scripts/amazon.js
+++ javascript-amazon-project/scripts/amazon.js
@@ -1,32 +1,41 @@
-const products = [
-  {
-    iamge: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      starts: 4.5,
-      count: 87,
-    },
-    priceCents: 1090,
-  },
-  {
-    iamge: "images/products/intermediate-composite-basketball.jpg",
-    name: "Intermediate Size Basketball",
-    rating: {
-      starts: 4,
-      count: 127,
-    },
-    priceCents: 2095,
-  },
-  {
-    iamge: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
-    rating: {
-      starts: 4.5,
-      count: 56,
-    },
-    priceCents: 799,
-  },
-];
+// const products = [
+//   {
+//     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+//     name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+//     rating: {
+//       starts: 4.5,
+//       count: 87,
+//     },
+//     priceCents: 1090,
+//   },
+//   {
+//     image: "images/products/intermediate-composite-basketball.jpg",
+//     name: "Intermediate Size Basketball",
+//     rating: {
+//       starts: 4,
+//       count: 127,
+//     },
+//     priceCents: 2095,
+//   },
+//   {
+//     image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+//     name: "Adults Plain Cotton T-Shirt - 2 Pack",
+//     rating: {
+//       starts: 4.5,
+//       count: 56,
+//     },
+//     priceCents: 799,
+//   },
+//   {
+//     image: "images/products/black-2-slot-toaster.jpg",
+//     name: "2 Slot Toaster - Black",
+//     rating: {
+//       starts: 5,
+//       count: 2197,
+//     },
+//     priceCents: 1899,
+//   },
+// ];
 let productHtml = "";
 products.forEach((prodcut, index) => {
   productHtml += `
@@ -34,7 +43,7 @@ products.forEach((prodcut, index) => {
           <div class="product-image-container">
             <img
               class="product-image"
-              src="${prodcut.iamge}"
+              src="${prodcut.image}"
             />
           </div>
 
@@ -45,7 +54,7 @@ products.forEach((prodcut, index) => {
           <div class="product-rating-container">
             <img
               class="product-rating-stars"
-              src="images/ratings/rating-${prodcut.rating.starts * 10}.png"
+              src="images/ratings/rating-${prodcut.rating.stars * 10}.png"
             />
             <div class="product-rating-count link-primary">${
               prodcut.rating.count

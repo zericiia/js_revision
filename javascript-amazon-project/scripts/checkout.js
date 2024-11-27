0c7ddfb09c4b00f971a3adc9ba83cfb468fd3e1c
zericiia <karimbrahimi102@gmail.com>
2024-11-27 18:46:21 +0100

    create oop version of cart

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -1,5 +1,5 @@
 import { renderOrderSummery } from "./chekout/orderSummary.js";
 import { renderPaymentSummary } from "./chekout/paymentSummary.js";
-
+import "../data/cart-oop.js";
 renderOrderSummery();
 renderPaymentSummary();
--- javascript-amazon-project/scripts/utils/money.js
+++ javascript-amazon-project/scripts/utils/money.js
@@ -1,3 +1,3 @@
 export function formatCurency(priceCents) {
-  return (priceCents / 100).toFixed(2);
+  return (Math.round(priceCents) / 100).toFixed(2);
 }
